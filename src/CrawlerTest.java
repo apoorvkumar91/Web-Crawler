6a17d667af0ea4c49b6bf2811e58b21ae12dd653
Apoorv <apoorvkumar91@gmail.com>
2018-02-09 20:48:15 -0600

    Initial setup with main files

---
+++ src/CrawlerTest.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author apoorv
+ */
+public class CrawlerTest
+{
+    /**
+     * This is our test. It creates a spider (which creates spider legs) and crawls the web.
+     * 
+     * @param args
+     *            - not used
+     */
+    public static void main(String[] args)
+    {
+        Crawler spider = new Crawler();
+        spider.search("http://arstechnica.com/", "computer");
+    }
+}
